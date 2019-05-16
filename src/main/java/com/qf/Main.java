bb61348f2f976ed54f50d31bd1021fd58ba9caeb
prs <prs@qq.com>
2019-05-16 15:54:38 +0800

    修改了一些内容

--- src/main/java/com/qf/Main.java
+++ src/main/java/com/qf/Main.java
@@ -8,7 +8,8 @@ package com.qf;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("hello git");
+        System.out.println("hello github");
     }
 
 }
